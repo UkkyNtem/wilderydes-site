d0e992e9a2f4d9ae7bb7822d4f5444f946015221
UkkyNtem <ukementem32@gmail.com>
2024-12-15 13:01:36 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_K25Zm4YJ', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '70flf1b3i2o5i6ot0lc5vfclij', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '70e3m898eju9gii2n2u70evku9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
